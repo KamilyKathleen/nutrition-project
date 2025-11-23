3030b993986cacb1128da3d1bb93c43ec3f8adf7
Kamily Freitas <freitas.kamily@hotmail.com>
2025-11-23 19:35:11 -0300

    Register screen and new texts on Home, FAQ and Blog

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -22,12 +22,12 @@ export default function Card({ image, description, title, subtitle, page, button
                 <Image
                     src={image}
                     alt="Card Image"
-                    className="w-full h-[230px] object-cover mb-2"
+                    className="w-full h-[230px] object-cover mb-2 rounded-t-lg"
                 />
             )}
             <div className='w-[90%] mx-auto p-4'>
-                <h3 className="ml-2 text-xl ">{title}</h3>
-                <h3 className="ml-2 text-xl text-petroleumGreen font-bold uppercase">{subtitle}</h3>
+                <h3 className="text-xl ">{title}</h3>
+                <h3 className="text-xl text-petroleumGreen font-bold uppercase">{subtitle}</h3>
                 <hr className='my-4 text-gray-200' />
                 <p className="">{description}</p>
                 <div className="flex justify-center my-4">
@@ -35,7 +35,6 @@ export default function Card({ image, description, title, subtitle, page, button
                         href={page}
                         className="border-2 text-petroleumGreen border-mintGreen hover:bg-petroleumGreen hover:border-petroleumGreen hover:text-white text-sm font-semibold py-2 px-3 rounded-md flex items-center"
                     >
-
                         <FaPlus className="size-3 mr-2" />
                         {button}
                     </Link>
